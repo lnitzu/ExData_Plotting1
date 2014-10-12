4bffd3e7db2609c3e65950f10bda962cb2accdaa
lnitzu <lnitzu@hotmail.com>
2014-10-12 16:08:25 -0700

    the second part

---
+++ RPlot3.R
@@ -0,0 +1,29 @@
+
+
+setwd("C:\\Users\\lnitu\\Documents\\COURSES\\COURSERA\\ExploratoryDataAnalysis\\exdata_data_household_power_consumption")
+epc<-read.csv('household_power_consumption.txt', sep=';')
+
+wd<-as.Date(epc$Date, "%d/%m/%Y")
+epc$Date<-wd
+
+
+myData<-epc[(epc$Date=='2007-02-01' | epc$Date=='2007-02-02'),]
+myData<-data.frame(DateTime=strptime(paste(as.character(myData$Date), myData$Time), format = "%Y-%m-%d %H:%M:%S"), myData)
+
+myData$Sub_metering_1<-as.numeric(myData$Sub_metering_1)
+myData$Sub_metering_2<-as.numeric(myData$Sub_metering_2)
+myData$Sub_metering_3<-as.numeric(myData$Sub_metering_3)
+
+png(file="RPlot3.png")
+with (myData, {
+  plot (DateTime, Sub_metering_1, type="l", col="black", xlab="", ylim=c(0,34), ylab="Energy sub metering")
+  lines (DateTime, Sub_metering_2, type="l", col="red")
+  lines (DateTime,Sub_metering_3, type="l", col="blue" )
+})
+dev.off()
+
+
+
+#png(file="RPlot2.png")
+#plot(myData$DateTime, myData$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+#dev.off()
---
+++ RPlot4.R
@@ -0,0 +1,38 @@
+setwd("C:\\Users\\lnitu\\Documents\\COURSES\\COURSERA\\ExploratoryDataAnalysis\\exdata_data_household_power_consumption")
+epc<-read.csv('household_power_consumption.txt', sep=';')
+
+
+
+
+wd<-as.Date(epc$Date, "%d/%m/%Y")
+epc$Date<-wd
+
+
+myData<-epc[(epc$Date=='2007-02-01' | epc$Date=='2007-02-02'),]
+myData<-data.frame(DateTime=strptime(paste(as.character(myData$Date), myData$Time), format = "%Y-%m-%d %H:%M:%S"), myData)
+
+myData$Sub_metering_1<-as.numeric(myData$Sub_metering_1)
+myData$Sub_metering_2<-as.numeric(myData$Sub_metering_2)
+myData$Sub_metering_3<-as.numeric(myData$Sub_metering_3)
+
+
+
+myData<-data.frame(DateTime <- strptime(paste(as.character(myData$Date), myData$Time), format = "%Y-%m-%d %H:%M:%S"), myData)
+
+
+png(file="RPlot4.png")
+
+par(mfrow=c(2,2))
+plot(myData$DateTime, myData$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+
+plot(myData$DateTime, myData$Voltage, type = "l", xlab = "", ylab = "Voltage")
+
+with (myData, {
+  plot (DateTime, Sub_metering_1, type="l", col="black", xlab="datetime", ylim=c(0,34), ylab="Energy sub metering")
+  lines (DateTime, Sub_metering_2, type="l", col="red")
+  lines (DateTime,Sub_metering_3, type="l", col="blue" )
+})
+
+plot(myData$DateTime, myData$Global_reactive_power, type = "l", xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
